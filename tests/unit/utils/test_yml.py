f6be34253f99bacce414d2e7c0e543ec76a90ee5
Dat Nguyen <dat@infinitelambda>
2023-11-02 17:30:14 +0700

    feat: test tasks

--- tests/unit/utils/test_yml.py
+++ tests/unit/utils/test_yml.py
@@ -1,18 +1,20 @@
 from unittest import mock
-from diqu.utils import yml
-import pytest
+
 import click
+import pytest
+
+from diqu.utils import yml
 
 
 class TestYml:
     def test_load_yml_ok(self):
         with mock.patch(
             "builtins.open",
-            mock.mock_open(read_data=str.encode("data", encoding="utf-8"))
+            mock.mock_open(read_data=str.encode("data", encoding="utf-8")),
         ) as mock_file:
             assert "data" == yml.load(file_path="path/to/irrelevant.yml")
         mock_file.assert_called_with("path/to/irrelevant.yml", "r")
-    
+
     def test_load_yml_failed(self):
         with pytest.raises(click.FileError):
             yml.load(file_path="invalid/path/to/irrelevant.yml")

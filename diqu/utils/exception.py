4151de7d451aca0327a64d4c772f51829b8bd1c8
Dat Nguyen <dat@infinitelambda>
2024-03-04 14:46:37 +0700

    chore: more useful error content

--- diqu/utils/exception.py
+++ diqu/utils/exception.py
@@ -8,7 +8,7 @@ def handle_module_errors(module: str, msg: str = ""):
     try:
         yield
     except Exception as e:
-        raise click.UsageError(f"Module {module} could not be found ({msg or str(e)})")
+        raise click.UsageError(f"Module {module} could not be found ({msg}. Details: {str(e)})")
 
 
 @contextlib.contextmanager
@@ -16,7 +16,7 @@ def handle_file_errors(file: str, msg: str = ""):
     try:
         yield
     except Exception as e:
-        raise click.FileError(f"File {file} could not be read ({msg or str(e)})")
+        raise click.FileError(f"File {file} could not be read ({msg}. Details: {str(e)})")
 
 
 @contextlib.contextmanager
@@ -24,4 +24,4 @@ def handle_config_errors(config: str, msg: str = ""):
     try:
         yield
     except Exception as e:
-        raise click.BadParameter(f"{config} is not reachable ({msg or str(e)})")
+        raise click.BadParameter(f"{config} is not reachable ({msg}. Details: {str(e)})")
